fb4e0e826859786684dbc4fbe53ac4422fed9401
Iddqd <bigsobolev@yandex.ru>
2021-07-14 16:41:38 +0300

    3. Арифметические операции. [#185]

--- src/ru/job4j/Info.java
+++ src/ru/job4j/Info.java
@@ -3,5 +3,9 @@ package ru.job4j;
 public class Info {
     public static void main(String[] args) {
         System.out.println("14.07.2021");
+        int age = 18;
+        System.out.println(age);
+        double length = 78.0332;
+        System.out.println(length);
     }
 }
