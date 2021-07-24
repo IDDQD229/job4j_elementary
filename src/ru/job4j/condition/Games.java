4692b7c6305fe77caee68b9739aeff24cadb52ba
Iddqd <bigsobolev@yandex.ru>
2021-07-24 15:19:02 +0300

    4.3.1. Оператор && [#333783]

--- src/ru/job4j/condition/Games.java
+++ src/ru/job4j/condition/Games.java
@@ -11,5 +11,8 @@ public class Games {
 
     public static void main(String[] args) {
         Games.permission(true, true);
+        Games.permission(true, false);
+        Games.permission(false, true);
+        Games.permission(false, false);
     }
 }
