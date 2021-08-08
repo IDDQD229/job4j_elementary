3babf9538ff052ec3305667680d264e26d1a916b
Iddqd <bigsobolev@yandex.ru>
2021-08-08 15:13:42 +0300

    6.0.2. Заполнение массива. [#226300]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -13,6 +13,9 @@ public class ArrayDefinition {
         names[1] = "Nikolay";
         names[2] = "Oleg";
         names[3] = "Vladimir";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
         System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
