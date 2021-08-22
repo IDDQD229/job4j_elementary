a215c3e426b191f87f825bf45370973ecc764b28
IDDQD <bigsobolev@yandex.ru>
2021-08-22 12:28:41 +0300

    6.5. Поиск индекса в диапазоне. [#156318]

--- src/ru/job4j/array/FindLoop.java
+++ src/ru/job4j/array/FindLoop.java
@@ -9,6 +9,17 @@ public class FindLoop {
                 break;
             }
         }
-         return rst;
+        return rst;
+    }
+    public static int indexOf(int[] data, int el, int start, int finish) {
+        int rst = - 1;
+        for (int index = start; index < finish; index++) {
+            if (data[index]== el) {
+                rst = index;
+                break;
+            }
+
+        }
+        return rst;
     }
 }
